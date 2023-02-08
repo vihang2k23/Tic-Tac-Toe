debc3673ae06868ce50400fdec6d56b77300ae27
vihang2k23 <vihangp@zignuts.com>
2023-02-08 17:44:10 +0530

    change_folder_structure

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ function checkWinner() {
   for (let i = 0; i < winConditions.length; i++) {
     const condition = winConditions[i];
     console.log(condition, "condition");
-    
+
     const cellA = options[condition[0]];
     const cellB = options[condition[1]];
     const cellC = options[condition[2]];
